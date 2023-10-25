0bc7edaedebe51fc6dbfb9cbf539d5deef08b624
Your Name <s026134@students.lmsd.org>
2023-10-25 14:23:49 -0400

    return of 0

--- app/fib.py
+++ app/fib.py
@@ -9,4 +9,6 @@ def fibonacci(position):
     return None
   if(position == 1 or position == 2):
     return 1
+  if(position == 0):
+    return 0
   return fibonacci(position - 1) + fibonacci(position - 2)
